2f47a27f625612c1abb5c0f9208dbf218dae8671
Navdeep-Singh-Hub <nonavi080@gmail.com>
2025-11-21 14:26:38 +0530

    updated the oicons

--- constants/images.ts
+++ constants/images.ts
@@ -1,14 +1,13 @@
+import highlight from "@/assets/icons/arrow.png";
+import rankingGradient from "@/assets/icons/games.png";
+import tapNowIcon from "@/assets/icons/gamesf.png";
+import { default as logo, default as trophyIcon } from "@/assets/icons/logo.png";
+import tapIcon from "@/assets/icons/play.png";
+import streakIcon from "@/assets/icons/save.png";
+import timerIcon from "@/assets/icons/settings.png";
+import starIcon from "@/assets/icons/star.png";
 import xpIcon from "@/assets/images/10xp.png";
 import bg from "@/assets/images/bg.png";
-import highlight from "@/assets/images/highlight.png";
-import rankingGradient from "@/assets/images/rankingGradient.png";
-import starIcon from "@/assets/images/star.png";
-import streakIcon from "@/assets/images/streak.png";
-import tapIcon from "@/assets/images/tap.png";
-import tapNowIcon from "@/assets/images/tapnow.png";
-import timerIcon from "@/assets/images/timer.png";
-import trophyIcon from "@/assets/images/trophy.png";
-import logo from "@/assets/images/logo.png";
 
 export const images = {
   bg,
